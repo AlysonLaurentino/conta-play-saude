8e72917b2953d645a2db11efce0a3a17ae2c8f69
Alyson Patrick <alyson.laurentino@outlook.com>
2021-10-01 09:38:20 -0300

    Atualização

--- PlataformaCartao/CadastrarParceiro.js
+++ PlataformaCartao/CadastrarParceiro.js
@@ -64,9 +64,9 @@ for(var i = 0; i<form.categoria.length;i++){
 
     var parceriaOf = {
 
-                nomeFantasia: form.fantasia.value,
-                descricaoPromocao:form.descricaoPromo.value,
-                funcionamento:form.funcionamento.value,
+        nomeFantasia: form.fantasia.value,
+        funcionamento:form.funcionamento.value,
+        descricaoPromocao: form.descricaoPromo.value,
                
                 endereco: 
                     {
