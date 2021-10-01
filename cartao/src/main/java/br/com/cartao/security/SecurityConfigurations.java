21ad8485783b547447310b15eb607a2ab92a6547
Alyson Patrick <alyson.laurentino@outlook.com>
2021-09-30 22:26:57 -0300

    Atualição Front e Bake

--- cartao/src/main/java/br/com/cartao/security/SecurityConfigurations.java
+++ cartao/src/main/java/br/com/cartao/security/SecurityConfigurations.java
@@ -46,13 +46,11 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.antMatchers("/auth").permitAll()
-				.antMatchers("/loja").permitAll()
 				.antMatchers("/login").permitAll()
-				.antMatchers("/loginTeste").permitAll()
-                .antMatchers("/parceria").permitAll()
-				.antMatchers("/dawlond").permitAll()
-				.antMatchers("/auth").permitAll()
-				.antMatchers("/dawlond/{someID}").permitAll()
+//				.antMatchers("/loja").permitAll()
+//                .antMatchers("/parceria").permitAll()
+//				.antMatchers("/dawlond").permitAll()
+//				.antMatchers("/dawlond/{someID}").permitAll()
 
 				
 				//// .and().csrf().disable().headers().frameOptions().disable()
